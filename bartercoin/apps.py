353e5a35e71be4c6233939ecdad2e358d9307765
MackRoe <elaine.music@students.makeschool.com>
2019-12-02 11:20:03 -0800

    remove django & restyle navbar

--- bartercoin/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BartercoinConfig(AppConfig):
-    name = 'bartercoin'
--- bartercoin/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.PageList, name='post_list'),
-    path('offer/', views.PageDetailView, name="page")
-]
